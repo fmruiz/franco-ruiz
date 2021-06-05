6e6e04a7e00fb8cc5f24dfb6fccfbc58e2a97800
FrancoMRuiz <x.francoruiz@gmail.com>
2021-06-05 04:22:31 -0300

    add transition to title

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -10,32 +10,42 @@ export const MainContainer = styled.div`
   align-items: center;
   position: relative;
   overflow: hidden;
+  user-select: none;
 
   /* change about title */
   .change__about::after {
     content: "Hello.";
+    transition: all 0.5s;
   }
   .change__about:hover:after {
     content: "About";
+    padding-left: 50px;
+    transition: all 0.5s;
   }
   /* change work title */
   .change__work::after {
     content: "I am";
+    transition: all 0.5s;
   }
   .change__work:hover:after {
     content: "Work";
+    padding-left: 50px;
+    transition: all 0.5s;
   }
   /* change contact title */
   .change__contact::after {
     content: "Franco";
+    transition: all 0.5s;
   }
   .change__contact:hover:after {
     content: "Contact";
+    padding-left: 50px;
+    transition: all 0.5s;
   }
 `;
 
 export const MainTitle = styled.h1`
-  width: 60%;
+  width: 65%;
   margin: 0 auto;
   line-height: 200px;
 `;
