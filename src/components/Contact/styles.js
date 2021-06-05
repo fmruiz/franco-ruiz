19618c80a6f19452a04a59193753d690f7646f3b
FrancoMRuiz <x.francoruiz@gmail.com>
2021-06-05 20:35:19 -0300

    add contact content

--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const ContactContainer = styled.div`
-  height: 100vh;
   background-color: #000000;
   display: flex;
   flex-direction: column;
@@ -13,5 +12,57 @@ export const LinkContainer = styled.div`
   .link {
     top: 60px;
     left: 75%;
+  }
+`;
+
+export const ContactTitle = styled.h3`
+  color: white;
+  margin: 0;
+  font-size: 80px;
+  margin-top: 450px;
+  margin-left: 200px;
+`;
+
+export const ContactContent = styled.div`
+  margin-left: 200px;
+  margin-top: 300px;
+  width: 55%;
+`;
+
+export const ContactContentTitle = styled.h3`
+  margin: 0;
+  color: white;
+  font-size: 60px;
+`;
+
+export const ContactContentSubTitle = styled.p`
+  margin-bottom: 0;
+  color: white;
+  font-size: 30px;
+`;
+
+export const ContactContentParagraph = styled.p`
+  margin-bottom: 0;
+  color: white;
+  font-size: 23px;
+`;
+
+export const ContactContentLinksContainer = styled.a`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 300px;
+`;
+
+export const ContactContentLink = styled.a`
+  margin-bottom: 0;
+  color: #ff1744;
+  font-size: 23px;
+  text-decoration: none;
+  margin-top: 23px;
+  transition: all 0.5s;
+
+  :hover {
+    padding-left: 30px;
+    transition: all 0.5s;
   }
 `;
