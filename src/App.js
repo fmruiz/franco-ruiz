e9560bac356a9c6e503beafc69823f361b9821d4
FrancoMRuiz <x.francoruiz@gmail.com>
2021-06-07 00:31:27 -0300

    portfolio v2

--- src/App.js
+++ src/App.js
@@ -1,42 +1,7 @@
 import React from "react";
-import { Main } from "./components/Main/Main";
-import { About } from "./components/About/About";
-import { Work } from "./components/Work/Work";
-import { Contact } from "./components/Contact/Contact";
-import { Fenix } from "./components/Fenix/Fenix";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import AnimatedCursor from "react-animated-cursor";
-import { Personal } from "./components/Personal/Personal";
 
 export const App = () => (
-  <Router>
-    <AnimatedCursor
-      innerSize={8}
-      outerSize={8}
-      color="101, 31, 255"
-      outerAlpha={0.5}
-      innerScale={2}
-      outerScale={5}
-    />
-    <Switch>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/work">
-        <Work />
-      </Route>
-      <Route exact path="/personal">
-        <Personal />
-      </Route>
-      <Route exact path="/fenix">
-        <Fenix />
-      </Route>
-      <Route path="/contact">
-        <Contact />
-      </Route>
-      <Route path="/">
-        <Main />
-      </Route>
-    </Switch>
-  </Router>
+  <>
+    <h1>Hola mundo</h1>
+  </>
 );
